cf08e30efa2c9372cf43213ec6b7a855a88dfc20
aerinp <s5253812@gmail.com>
2024-02-06 17:39:23 -0800

    Fix header

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ const toggleBtn = document.querySelector('.nav_toggle');
 const menu = document.querySelector('.nav_menu');
 const icons = document.querySelector('.nav_icons');
 const title = document.querySelector('.nav_title');
+const header = document.querySelector('header');
 
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
     title.classList.toggle('active');
+    header.classList.toggle('active');
 });
 
 const showDialog = id => {
